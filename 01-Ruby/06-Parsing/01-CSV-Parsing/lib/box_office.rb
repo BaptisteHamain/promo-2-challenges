3439a4fd999df291090f5cb0e29679b7a05eefe5
BaptisteHamain <baptistehamain@icloud.com>
2014-04-15 17:58:42 +0200

    First attempt, solved it

--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -3,4 +3,20 @@ require 'csv'
 
 def most_successfull(number, max_year, file_name)
   #TODO: return the number most successfull movies max_year
+  array = []
+
+  CSV.foreach(file_name, {encoding: "iso-8859-1:UTF-8"} ) do |row|
+
+    info = {
+      name: row[0],
+      year: row[1].to_i,
+      earnings: row[2].to_i
+    }
+
+    array << info
+  end
+
+  array.keep_if { |info| info[:year] < max_year }
+  array.take(number)
+
 end

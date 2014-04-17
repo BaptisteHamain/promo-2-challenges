07be1b8e91e5fae784233bc13abf1b6d3bc8677e
BaptisteHamain <baptistehamain@icloud.com>
2014-04-17 12:33:40 +0200

    first

--- 02-OOP/03-Basic-OOP/lib/orange_tree.rb
+++ 02-OOP/03-Basic-OOP/lib/orange_tree.rb
@@ -1,52 +1,63 @@
 class OrangeTree
- # TODO: Implement all the specs defined in the README.md :)
+  attr_reader :height, :fruits
+
   def initialize
     @fruits = 0
-    @age = 0
     @height = 0
-    @fruit_count = 0
+    @age = 0
+    @dead = false
   end
-
-  attr_reader :height, :age, :height, :fruit_count
 
  # Age en fonction du temps qui passe
   def one_year_passes!
-    fruit_count = 0
+    unless dead?
+      grow_up
+      grow_fruits
+      may_die?
+    end
+
     @age += 1
   end
 
-  # Définition d'un arbre qui vit ou non
-  def dead?
-    if @age <= 50
-      false
-    else
-      true
-    end
+ #Définition sur le fait qu'une personne puisse prendre un fruit
+  def pick_a_fruit!
+    @fruits = @fruits - 1 if @fruits > 0
   end
 
- # Définition de l'arbre qui grandit de 1m jusqu'à 10 ans
-  def height
-    if @age < 10
-      @height = @age
-    else
-      @height = 10
-    end
+
+  def dead?
+    @dead
   end
+
+  private
+
+# Définition de l'arbre qui grandit de 1m jusqu'à 10 ans
+    def grow_up
+      @height += 1 if can_still_grow_up?
+    end
+
+    def can_still_grow_up?
+      @age < 10
+    end
 
  # On définit ici le nombre de fruits par ans
  # Données par l'arbre en fonction de son age
-  def fruits
-    if @age.to_i.between?(6, 10)
-      @fruits = 100 && @fruit_count = 100
-    elsif @age.to_i.between?(10, 15)
-      @fruits = 200 && @fruit_count = 200
-    else
-      @fruits = 0
-    end
-  end
+    def grow_fruits
+      @fruits = 0  # Fruits fall off
 
-#Définition sur le fait qu'une personne puisse prendre un fruit
-  def pick_a_fruit!
-    @fruit_count -= 1
-  end
+      if @age >= 5 && @age < 10
+        @fruits = 100
+      elsif @age >= 10 && @age < 15
+        @fruits = 200
+      end
+    end
+
+# Définition d'un arbre qui vit ou non
+    def may_die?
+      if @age >= 100
+        @dead = true
+      elsif @age >= 50
+        @dead = rand(10) == 0 # 10% of chance to die
+      end
+    end
 end

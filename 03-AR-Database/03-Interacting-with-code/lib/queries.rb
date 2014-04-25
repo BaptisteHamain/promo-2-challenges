cc2ab8f52d795d3d90404ee2a456ad29b3c1d1bb
BaptisteHamain <baptistehamain@icloud.com>
2014-04-25 13:13:00 +0200

    First attempt, solved it

--- 03-AR-Database/03-Interacting-with-code/lib/queries.rb
+++ 03-AR-Database/03-Interacting-with-code/lib/queries.rb
@@ -4,21 +4,18 @@ require 'sqlite3'
 DATABASE_PATH = File.join(File.dirname(__FILE__), 'db/jukebox.sqlite')
 db = SQLite3::Database.new(DATABASE_PATH)
 
-database_path = File.join(File.dirname(__FILE__), 'db/jukebox.sqlite')
-db = SQLite3::Database.new(database_path)
-
 def number_of_rows(db, table_name)
-  db.execute( " SELECT COUNT(*) FROM #{table_name} " )
+  db.execute( " SELECT COUNT(*) FROM #{table_name} " ).first[0]
 end
 
 def sorted_artists(db)
-  db.execute( "SELECT Name FROM Artist ORDER BY Name ")
+  track = db.execute( "SELECT Name FROM Artist ORDER BY Name ").flatten
 end
 
 def love_tracks(db)
-  db.execute( "SELECT Name FROM Track WHERE Name LIKE '%love%' ")
+  db.execute( "SELECT Name FROM Track WHERE Name LIKE '%love%' ").flatten
 end
 
 def long_tracks(db, min_length)
-  db.execute( "SELECT Name FROM Track WHERE Milliseconds > #{min_length}*60000 ")
+  db.execute( "SELECT Name FROM Track WHERE Milliseconds > #{min_length}*60000 ").flatten
 end

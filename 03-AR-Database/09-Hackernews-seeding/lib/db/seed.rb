0e601b3d911e2f75b55fd655e9f28714a344e2d1
BaptisteHamain <baptistehamain@icloud.com>
2014-04-29 11:43:53 +0200

    exo 9

--- 03-AR-Database/09-Hackernews-seeding/lib/db/seed.rb
+++ 03-AR-Database/09-Hackernews-seeding/lib/db/seed.rb
@@ -1,12 +1,43 @@
 require_relative "../config/application"
 require_relative "../models/post"
 require_relative "../models/user"
+require 'open-uri'
+require 'nokogiri'
 
 # Seed you database with post data from hacker news
 # And fake users using Faker gem
 
 puts "Seeding database..."
 
-#TODO: Your code goes here 
+User.destroy_all
+Post.destroy_all
+
+#TODO: Your code goes here
 # 1. create fake users
+users = []
+5.times do
+  user = User.create(name: Faker::Name.name, email: Faker::Internet.email)
+  users << user
+end
+
 # 2. Scrape news from https://news.ycombinator.com/ and associate posts to your existing users
+
+response = open("https://news.ycombinator.com/")
+
+doc = Nokogiri::HTML(response) #:: signifie que l'on se situe dans le sous-module HTML de Nokogiri
+
+doc.search('td.title>a').each do |element|
+x = element.parent.parent.next_sibling
+  if x != nil
+    y = x.search("td.subtext>span").first
+    if y != nil
+      Post.create({
+       name: element.inner_text,
+       date: Time.now,
+       source_url: element['href'],
+       user: users.sample,
+       rating: y.inner_text
+      })
+    end
+  end
+end
--- 03-AR-Database/09-Hackernews-seeding/lib/interface.rb
+++ 03-AR-Database/09-Hackernews-seeding/lib/interface.rb
@@ -3,3 +3,58 @@ require_relative 'models/post'
 require_relative 'models/user'
 
 #TODO: build your interface with login and CRUD tasks on the posts
+
+def ask_and_get(param)
+  puts "What is the #{param} of your piece of news?"
+  gets.chomp
+end
+
+while true
+  logged_in = false
+
+  until logged_in
+    puts "Please login with your <id>"
+    id = gets.chomp.to_i
+
+    logged_in = true
+
+    begin
+      user = User.find(id)
+    rescue
+      puts "Error!"
+      logged_in = false
+    end
+  end
+
+  puts "Hey #{user.name}, what do you want to do today? Enter <task_id>"
+  puts "1. Create a post"
+  puts "2. Read your posts"
+  puts "3. Delete all posts"
+  puts "4. Exit"
+
+  choice =  gets.chomp.to_i
+
+  case choice
+  when 1
+    name = ask_and_get("name")
+    source_url = ask_and_get("source url")
+    rating = ask_and_get("rating")
+    post = { name: name, source_url: source_url, date: Time.now, rating: rating }
+    #TODO: use ActiveRecord to add a new post for the user logged in!
+    user.posts.create(post)
+  when 2
+    #TODO: use ActiveRecord to get all posts of the current user
+
+    user.posts.each do |post|
+      puts post
+    end
+  when 3
+    #TODO: use ActiveRecord to delete all posts of current user
+    user.post.destroy_all
+  when 4
+
+  when 5
+    break
+  end
+
+end

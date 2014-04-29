f935752053da19ace91cd7230e0c90c2a5f664d1
BaptisteHamain <baptistehamain@icloud.com>
2014-04-29 15:50:40 +0200

    exo 1à OK

--- 03-AR-Database/10-Hackernews-validations/lib/models/post.rb
+++ 03-AR-Database/10-Hackernews-validations/lib/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+
+  validates :name, uniqueness: true
+  validates :rating, numericality: { only_integer: true}
+  validates :date, :source_url, :user, presence: true
+
 end
+
+#Make sure all fields in posts aure 'required'
+#Make sure thzt name is unique for posts
+#(i.e you cannot have two posts with the same name in your app)
+#Make sure rating >=0
--- 03-AR-Database/10-Hackernews-validations/lib/models/user.rb
+++ 03-AR-Database/10-Hackernews-validations/lib/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
   has_many :posts
+
+  validates :email, uniqueness: true
+  validates :email, format: {with: \A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+          message: "is not an email"}
+  validates :name, presence: true
 end
+
+#Make sure all fields in users aure 'required'
+#Make sure email is 'unique' for users
+#Make sure email is 'valid' (xxxxx@xxx)
